1fd783853a9a9e1a9decc537a71b967338eb501c
Harry-cheong <harrycheongjunkai@gmail.com>
2025-05-06 09:40:10 +0800

    fixed dropdown navbar

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -21,21 +21,25 @@
 
     <v-menu v-else>
       <template v-slot:activator="{ props }">
-        <v-btn
-          class="my-auto ml-auto px-10"
-          v-bind="props"
-          @click="showMenu = !showMenu"
-        >
+        <v-btn class="my-auto ml-auto px-10" @click="showMenu = !showMenu">
           <v-icon v-if="!showMenu" size="x-large">mdi-menu</v-icon>
           <v-icon v-else size="medium">mdi-triangle-down</v-icon>
         </v-btn>
       </template>
     </v-menu>
   </v-app-bar>
+
   <v-sheet
     v-if="showMenu"
     v-show="showMenu"
-    style="width: 100%; padding-top: 130px; height: 460px"
+    style="
+      width: 100%;
+      padding-top: 130px;
+      position: fixed;
+      z-index: 10;
+      background-color: black;
+      opacity: 0.8;
+    "
   >
     <v-btn
       v-for="page in pages"
@@ -43,7 +47,7 @@
       @click="showMenu = !showMenu"
       variant="outlined"
       class="text-center pa-8"
-      style="background-color: white; width: 100%"
+      style="width: 100%; color: white"
       >{{ page.title }}</v-btn
     >
   </v-sheet>
